072d46dc4bcce67eaff6db2c0a2e98b29aea4280
Massimo Cavallaro <cavallaromass@gmail.com>
2017-09-03 16:58:22 +0100

    PEP compliant

--- bookletpages.py
+++ bookletpages.py
@@ -1,14 +1,15 @@
-import sys
 
-def doppioanagnipanoramic(number):
-    ''' Build the index vector con doppio Anagni "panoramic" '''
+def doppioAnagniPanoramic(number):
+    '''
+    Build the index vector, con doppio Anagni "panoramic"
+    '''
     index1 = range(1, number / 2 + 1)
     index2 = range(number, number / 2, -1)
     index = []
     i = 0
     index.append(index2[0])
     while True:
-        # rearrange con DOPPIO ANAGNI
+        # rearrange, con DOPPIO ANAGNI
         index.append(index1[i])
         index.append(index1[i+1])
         i = i + 1
@@ -20,15 +21,17 @@ def doppioanagnipanoramic(number):
     return index
 
 
-def doppioanagnicrazy(number, pages, max_tmp):
-    ''' Build the index vector con doppio Anagni "crazy" '''
+def doppioAnagniCrazy(number, pages, max_tmp):
+    '''
+    Build the index vector, con doppio Anagni "crazy"
+    '''
     index1 = range(1 + max_tmp, number / 2 + 1 + max_tmp)
     index2 = ['NA' for _ in xrange(number - pages)] + range(pages + max_tmp, number / 2 + max_tmp, -1)
     index = []
     i = 0
     index.append(index2[0])
     while True:
-        # rearrange con DOPPIO ANAGNI
+        # rearrange, con DOPPIO ANAGNI
         index.append(index1[i])
         index.append(index1[i+1])
         i = i + 1
@@ -39,38 +42,37 @@ def doppioanagnicrazy(number, pages, max_tmp):
         index.append(index2[i])
     return index
 
+def main(n_pages_booklet, total_pages):
 
-pages_booklet = int(sys.argv[1])
-total_pages = int(sys.argv[2])
+    lista_in = doppioAnagniPanoramic(n_pages_booklet)
+    lista = lista_in[:]
 
-lista_in = doppioanagnipanoramic(pages_booklet)
-lista = lista_in[:]
+    i=0
+    while True:
+        i = i + 1
+        lista_tmp = [p + n_pages_booklet*i for p in lista_in]
+        lista = lista + lista_tmp
+        if lista[n_pages_booklet*i] + n_pages_booklet > total_pages:
+            break
 
-i=0
-while True:
-    i = i + 1
-    lista_tmp = [p + pages_booklet*i for p in lista_in]
-    lista = lista + lista_tmp
-    if lista[pages_booklet*i] + pages_booklet > total_pages:
-        break
+    for l in lista:
+        print("%d," % l)
 
-for l in lista:
-#    print 
-    sys.stdout.write("%d," % l)
-#    sys.stdout.write(', ')
+    max_tmp = lista[n_pages_booklet*i]
 
-max_tmp = lista[pages_booklet*i]
+    pages_left = total_pages - max_tmp
 
-pages_left = total_pages - max_tmp
+    print("\nThere are %d pages left," % pages_left)
 
-sys.stdout.write("\nThere are %d pages left," % pages_left)
+    last_group = (pages_left/4)*4 + 4*bool(pages_left%4)
 
-last_group = (pages_left/4)*4 + 4*bool(pages_left%4)
+    print(" make another booklet of %d pages\n" % last_group)
 
-sys.stdout.write(" make another booklet of %d pages\n" % last_group)
+    last_booklet = doppioAnagniCrazy(last_group, pages_left, max_tmp)
 
-last_booklet = doppioanagnicrazy(last_group, pages_left, max_tmp)
+    for l in last_booklet:
+        print(str(l) + ',')
 
-for l in last_booklet:
-    sys.stdout.write(str(l) + ',')
-
+if __name__ == '__main__':
+    import sys
+    main(int(sys.argv[1]), int(sys.argv[2]))
